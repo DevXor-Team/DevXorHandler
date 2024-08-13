a2102a60e7896b003b7c5676f94e4748432db72a
Abd EL-Rahman Walid Hassan <97101497+Abdelrahmanwalidhassan@users.noreply.github.com>
2024-08-13 11:03:40 +0300

    repo back old state

--- src/handlers/ComponentsActionLoader.js
+++ src/handlers/ComponentsActionLoader.js
@@ -1,62 +1,57 @@
-const ascii = require("ascii-table");
-const c = require("chalk");
-const path = require("path");
-const { recursiveReadDirSync } = require("@utils/class/utils");
-const glob = require("glob");
+
+const ascii = require('ascii-table');
+const c = require('chalk')
+const path = require('path');
+const { recursiveReadDirSync } = require('@utils/class/utils');
 
 /**
- *
- * @param {import("@root/src/base/baseClient")} client
+ * 
+ * @param {import("@root/src/base/baseClient")} client 
  * @param {string} directory directory containing the event files
  */
 module.exports = (client, directory) => {
-	let Component_table = new ascii(`Client All Components`);
 
-	Component_table.setHeading("Component flle", "IsEnabled", "Load Status");
-	// Component_table.setBorder('║', '=', '✥', '✥')
-	Component_table.setBorder("║", "═", "✥", "🌟");
-	Component_table.setAlign(0, ascii.CENTER);
-	Component_table.setAlign(1, ascii.LEFT);
-	Component_table.setAlign(2, ascii.LEFT);
-	Component_table.setAlign(3, ascii.CENTER);
+  let Component_table = new ascii(`Client All Components`);
 
-	glob(
-		"**/*.js",
-		{ cwd: path.join(__dirname, "../ComponentsAction") },
-		(err, files) => {
-			if (err) {
-				client.logger.error(err);
-				return;
-			}
+  Component_table.setHeading('Component flle', "IsEnabled", 'Load Status');
+  // Component_table.setBorder('║', '=', '✥', '✥')
+  Component_table.setBorder('║', '═', '✥', '🌟')
+  Component_table.setAlign(0, ascii.CENTER)
+  Component_table.setAlign(1, ascii.LEFT)
+  Component_table.setAlign(2, ascii.LEFT)
+  Component_table.setAlign(3, ascii.CENTER)
 
-			files.forEach((file) => {
-				const fileName = path.basename(file);
+  recursiveReadDirSync(directory).forEach(filePath => {
+    const fileName = path.basename(filePath);
 
-				/**
-				 * @type {import("@utils/types/baseComponent")}
-				 */
-				const Component = require(`../ComponentsAction/${file}`);
+    /**
+     * @type {import("@utils/types/baseComponent")} 
+     */
+    const Component = require(filePath);
 
-				try {
-					if (typeof Component !== "object") return;
-					client.loadComponent(Component);
-					delete require.cache[require.resolve("../ComponentsAction/" + file)];
+    try {
 
-					if (Component.enabled) Component_table.addRow(fileName, "✅", "✅");
-					else Component_table.addRow(fileName, "❌", "✅");
-				} catch (ex) {
-					if (Component.enabled)
-						Component_table.addRow(c.red(fileName), "❌", "❌");
+      if (typeof Component !== "object") return;
+      client.loadComponent(Component);
+      delete require.cache[require.resolve(filePath)];
 
-					client.logger.error(
-						`Failed to load ${fileName} Reason: ${ex.message}`,
-					);
-				}
-			});
-		},
-	);
+      if (Component.enabled)
+        Component_table.addRow(fileName, '✅', '✅');
+      else Component_table.addRow(fileName, '❌', '✅');
+    } catch (ex) {
 
-	client.logger.success(`Loaded ${client.ComponentsAction.size} Components`);
+      if (Component.enabled) Component_table.addRow(c.red(fileName), '❌', '❌');
 
-	console.log(Component_table.toString());
-};
+      client.logger.error(`Failed to load ${fileName} Reason: ${ex.message}`);
+    }
+
+  })
+
+  client.logger.success(`Loaded ${client.ComponentsAction.size} Components`);
+
+  console.log(Component_table.toString());
+
+
+}
+
+
--- src/handlers/EventsLoader.js
+++ src/handlers/EventsLoader.js
@@ -1,63 +1,55 @@
-const ascii = require("ascii-table");
-const c = require("chalk");
-const path = require("path");
-const { recursiveReadDirSync } = require("@utils/class/utils");
-const glob = require("glob");
+
+const ascii = require('ascii-table');
+const c = require('chalk')
+const path = require('path');
+const { recursiveReadDirSync } = require('@utils/class/utils');
 
 /**
- *
- * @param {import("@root/src/base/baseClient")} client
+ * 
+ * @param {import("@root/src/base/baseClient")} client 
  * @param {string} directory directory containing the event files
  */
-module.exports = async (client, directory) => {
-	// client.logger.log(`Loading events...`);
-	let success = 0;
-	let failed = 0;
+module.exports = (client, directory) => {
 
-	let Evtable = new ascii(`Client Events`);
+  // client.logger.log(`Loading events...`);
+  let success = 0;
+  let failed = 0;
 
-	Evtable.setHeading("event flle", "Load Status");
-	// Evtable.setBorder('║', '=', '✥', '✥')
-	Evtable.setBorder("║", "═", "✥", "🌟");
-	Evtable.setAlign(0, ascii.CENTER);
-	Evtable.setAlign(1, ascii.LEFT);
-	Evtable.setAlign(2, ascii.LEFT);
-	Evtable.setAlign(3, ascii.CENTER);
+  let Evtable = new ascii(`Client Events`);
 
-	glob("**/*.js", { cwd: path.join(__dirname, "../events") }, (err, files) => {
-		if (err) {
-			client.logger.error(err);
-			return;
-		}
+  Evtable.setHeading('event flle', 'Load Status');
+  // Evtable.setBorder('║', '=', '✥', '✥')
+  Evtable.setBorder('║', '═', '✥', '🌟')
+  Evtable.setAlign(0, ascii.CENTER)
+  Evtable.setAlign(1, ascii.LEFT)
+  Evtable.setAlign(2, ascii.LEFT)
+  Evtable.setAlign(3, ascii.CENTER)
+  recursiveReadDirSync(directory).forEach((filePath) => {
+    const file = path.basename(filePath);
+    try {
 
-		files.forEach((file) => {
-			const fileName = path.basename(file);
-			try {
-				const event = require("../events/" + file);
-				const eventName = event.name;
+      const event = require(filePath);
+      const eventName = event.name
 
-				if (event.once) {
-					client.once(eventName, (...args) => event.execute(client, ...args));
-				} else {
-					client.on(eventName, (...args) => event.execute(client, ...args));
-				}
-				Evtable.addRow(fileName, "✅");
+      if (event.once) {
+        client.once(eventName, (...args) => event.execute(client, ...args));
+      } else {
+        client.on(eventName, (...args) => event.execute(client, ...args));
+      }
+      Evtable.addRow(file, '✅');
 
-				delete require.cache[require.resolve(`../events/${file}`)];
-				success += 1;
-			} catch (ex) {
-				failed += 1;
-				Evtable.addRow(c.red(fileName), "❌");
+      delete require.cache[require.resolve(filePath)];
+      success += 1;
+    } catch (ex) {
+      failed += 1;
+      Evtable.addRow(c.red(file), '❌');
 
-				console.log(`loadEvent - ${file}`, ex);
-			}
-		});
-	});
-	console.log(Evtable.toString());
+      console.log(`loadEvent - ${file}`, ex);
+    }
+  });
 
-	client.logger.log(
-		`Loaded ${
-			success + failed
-		} events. Success (${success}) Failed (${failed})`,
-	);
-};
+  console.log(Evtable.toString());
+
+  client.logger.log(`Loaded ${success + failed} events. Success (${success}) Failed (${failed})`);
+
+}
